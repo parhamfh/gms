152aa13355f365dd954cfe54fa354aef2b19f2a8
Pärham Fazelzadeh <parham@epidemicsound.com>
2017-11-10 18:31:41 +0100

    Basic routing

--- gms.go
+++ gms.go
@@ -2,16 +2,21 @@ package main
 
 import (
     "fmt"
-    "net/http"
+    "html"
     "log"
+    "net/http"
+
     "github.com/gorilla/mux"
 )
-
 func main() {
-    http.HandleFunc("/adfad/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("Received request")
-        fmt.Fprintf(w, "Hey bro")
-    })
-    mux.NewRouter()
-    log.Print(http.ListenAndServe(":8080", nil))
+    router := mux.NewRouter().StrictSlash(true)
+    // Routes
+    router.HandleFunc("/", Index)
+    // Listen
+    log.Print(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Take care so long")
+    fmt.Fprintf(w, "Hey, b %q", html.EscapeString(r.URL.Path))
 }
