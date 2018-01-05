c70f5b1ad22418badbf574883ccc55cd839fc141
Pärham Fazelzadeh <parham@epidemicsound.com>
2018-01-05 15:01:00 +0100

    Be more explicit in response

--- handlers.go
+++ handlers.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "html"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    // fmt.Println("Received request", r)
-    fmt.Fprintf(w, "Index request: ", html.EscapeString(r.URL.Path))
+	// fmt.Println("Received request", r)
+	fmt.Fprintf(w, "Index request: ", html.EscapeString(r.URL.Path))
 }
 
 func TracksIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Tracks request")
+	fmt.Println("Tracks request")
 
-    tracks := Tracks{
-        Track{Name: "Sonic Blast!"},
-        Track{Name: "Pure Love"},
+	tracks := Tracks{
+		Track{Name: "Sonic Blast!"},
+		Track{Name: "Pure Love"},
+	}
+
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(201)
+
+    if err := json.NewEncoder(w).Encode(tracks); err != nil {
+        panic(err)
     }
-
-    json.NewEncoder(w).Encode(tracks)
 }
 
 func TracksShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    trackId := vars["trackId"]
-    fmt.Fprintln(w, "TracksShow request", trackId)
+	vars := mux.Vars(r)
+	trackId := vars["trackId"]
+	fmt.Fprintln(w, "TracksShow request", trackId)
 }
