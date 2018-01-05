92a1767bec5925e114a900d4b078868c53f6afc1
Pärham Fazelzadeh <parham@epidemicsound.com>
2018-01-05 15:01:08 +0100

    go fmt

--- gms.go
+++ gms.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
+
 func main() {
-    router := NewRouter()
-    log.Print(http.ListenAndServe(":8080", router))
+	router := NewRouter()
+	log.Print(http.ListenAndServe(":8080", router))
 }
